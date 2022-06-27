30bd48caa628e0aca71859412d3ba939a31bcc1c
st1xkz <83383474+st1xkz@users.noreply.github.com>
2022-06-27 22:50:46 +0000

    stuff

--- bobert/core/utils/color_logs.py
+++ bobert/core/utils/color_logs.py
@@ -2,8 +2,8 @@ import logging
 
 FMT = "[{levelname}] {name}: {message}"
 FORMATS = {
-    logging.DEBUG: f"\33[38;2;255;255;255m{FMT}\33[0m",
-    logging.INFO: f"\33[38;2;143;227;129m{FMT}\33[0m",
+    logging.DEBUG: f"\33[38;2;255;255;0m{FMT}\33[0m",
+    logging.INFO: f"\33[38;2;102;255;0m{FMT}\33[0m",
     logging.WARNING: f"\33[38;2;190;0;254m{FMT}\33[0m",
     logging.ERROR: f"\33[38;2;0;111;249m{FMT}\33[0m",
     logging.CRITICAL: f"\33[38;2;255;0;127m{FMT}\33[0m",
