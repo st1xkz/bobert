3f628cbb0a079fd06d7a50cde811bc845689040a
st1xkz <83383474+st1xkz@users.noreply.github.com>
2022-07-02 18:02:38 +0000

    Changed `purge.py`

--- bobert/plugins/moderation/purge.py
+++ bobert/plugins/moderation/purge.py
@@ -1,60 +1,47 @@
-import asyncio
+import datetime
 
 import hikari
 import lightbulb
-from lightbulb import errors
 
 purge_plugin = lightbulb.Plugin("purge")
+purge_plugin.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES))
 
 
 @purge_plugin.command
 @lightbulb.add_cooldown(10, 3, lightbulb.UserBucket)
-@lightbulb.add_checks(
-    lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES),
-    lightbulb.bot_has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES),
-)
 @lightbulb.option(
-    name="messages",
+    name="amount",
     description="the number of messages to delete",
     type=int,
+    max_value=100,
+    min_value=1,
     required=True,
 )
 @lightbulb.command(
     name="purge",
-    aliases=["clear"],
+    aliases=["clear", "c"],
     description="Deletes optional number of messages",
+    pass_options=True,
 )
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def cmd_purge(ctx: lightbulb.Context) -> None:
-    num_msgs = ctx.options.messages
-    channel = ctx.channel_id
+    if not ctx.guild_id:
+        await ctx.respond("This command can only be used in a server.")
+        return
 
-    if isinstance(ctx, lightbulb.PrefixContext):
-        await ctx.event.message.delete()
-
-    msgs = await ctx.bot.rest.fetch_messages(channel).limit(num_msgs)
-    await ctx.bot.rest.delete_messages(channel, msgs)
-
-    await ctx.respond(
-        f"<:yes:979187100907864104> **{len(msgs)}** messages were deleted",
-        delete_after=10,
-    )
-
-    await asyncio.sleep(10)
-
-
-@cmd_purge.set_error_handler
-async def on_purge_error(event: lightbulb.CommandErrorEvent) -> bool:
-    exc = event.exception.__cause__ or event.exception
-    ctx = event.context
-
-    if isinstance(exc, errors.NotEnoughArguments):
-        await ctx.respond(
-            "<:no:979185688933199892> You must specify the number of messages to delete."
+        # fetch messages that are not older than 14 days in the channel the command was invoked in
+        # messages older than 14 days be deleted by bots, so it's unnecessary
+        messages = (
+            await ctx.bot.rest.fetch_messages(ctx.guild_id)
+            .take_until(lambda m: datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=14) > m.created_at)
+            .limit(ctx.options.amount)
         )
-        return True
 
-    return False
+        if messages:
+            await ctx.bot.rest.delete_messages(ctx.channel_id, messages)
+            await ctx.respond(f"Purged {len(ctx.options.amount)} messages")
+        else:
+            await ctx.respond("<:no:979185688933199892> Couldn't find any messages younger than 14 days!")
 
 
 def load(bot: lightbulb.BotApp) -> None:
