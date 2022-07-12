8e7414a5d8d7376870e07a402ce4085adf9d1015
sticks <83383474+st1xkz@users.noreply.github.com>
2022-07-12 17:26:15 +0000

    starting on custom help command

---
+++ bobert/plugins/general/help.py
@@ -0,0 +1,31 @@
+"""
+import hikari
+import lightbulb
+
+
+class Help(lightbulb.BaseHelpCommand):
+    async def send_bot_help(self, ctx):
+        pass
+
+    async def send_plugin_help(self, ctx, plugin):
+        pass
+
+    async def send_command_help(self, ctx, cmd):
+        pass
+
+    async def send_group_help(self, ctx, group):
+        pass
+
+    async def object_not_found(self, ctx, obj):
+        pass
+
+
+def load(bot: lightbulb.BotApp) -> None:
+    bot.d.old_help_command = bot.help_command
+    bot.help_command = Help(bot)
+
+
+def unload(bot: lightbulb.BotApp) -> None:
+    bot.help_command = bot.d.old_help_command
+    del bot.d.old_help_command
+"""
