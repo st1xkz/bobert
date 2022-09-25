ad2b8d2577929d018e59b4b360ddbc687a468265
sticks <lucyabney101@gmail.com>
2022-09-25 19:05:00 +0000

    .

--- bobert/bot.py
+++ bobert/bot.py
@@ -14,7 +14,7 @@ from bobert.core.utils import color_logs
 load_dotenv()
 bot = lightbulb.BotApp(
     token=os.getenv("TOKEN"),
-    banner="assets",
+    banner="assets/assets",
     help_slash_command=True,
     ignore_bots=True,
     intents=hikari.Intents.ALL,
