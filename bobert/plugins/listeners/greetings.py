af39d3f90c316adb44c9debed551e68f7970cadb
sticks <lucyabney101@gmail.com>
2022-10-19 01:31:34 +0000

    change role and channel ids

--- bobert/plugins/listeners/greetings.py
+++ bobert/plugins/listeners/greetings.py
@@ -6,21 +6,21 @@ greetings_plugin = lightbulb.Plugin("greetings")
 
 @greetings_plugin.listener(hikari.MemberUpdateEvent)
 async def on_member_join_update(event: hikari.MemberUpdateEvent) -> None:
-    with open("bobert/db/users.json", "w+") as j:
-        j.write({str(event.member.id)})
-"""
     before = event.old_member
     after = event.member
-    role = 986449519615025202
-    if role in [r.id for r in after.get_roles()] and role not in [
-        r.id for r in before.get_roles()
-    ]:
-        await greetings_plugin.bot.rest.create_message(
-            900466082618425365,
-            f"You made it {after.mention}! Welcome to **{event.member.get_guild().name}**, enjoy your stay 💚",
-            user_mentions=True,
-        )
-"""
+    role = 993695690578464778
+
+    with open("bobert/db/users.json", "w+") as j:
+        j.write({str(event.member.id)})
+        
+        if role in [r.id for r in after.get_roles()] and role not in [
+            r.id for r in before.get_roles()
+        ]:
+            await greetings_plugin.bot.rest.create_message(
+                993567995936915536,
+                f"You made it {after.mention}! Welcome to **{event.member.get_guild().name}**, enjoy your stay 💚",
+                user_mentions=True,
+            )
 
 
 def load(bot: lightbulb.BotApp) -> None:
