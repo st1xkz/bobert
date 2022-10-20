48454306fef18ee65ebf3347ead0b1b0df20b30a
sticks <lucyabney101@gmail.com>
2022-10-20 04:04:43 +0000

    add aiosqlite

--- bobert/plugins/listeners/greetings.py
+++ bobert/plugins/listeners/greetings.py
@@ -1,7 +1,36 @@
+import aiosqlite
 import hikari
 import lightbulb
 
 greetings_plugin = lightbulb.Plugin("greetings")
+
+"""
+@greetings_plugin.listener(hikari.StartingEvent)
+async def setup(_):
+    async with aiosqlite.connect("memberlist.db") as db:
+        await db.execute("CREATE TABLE IF NOT EXISTS users (uid BIGINT);")
+        await db.commit()
+
+
+@greetings_plugin.listener(hikari.StartedEvent)
+async def add_old_users(_):
+    mems = greetings_plugin.cache.get_members_view_for_guild(993565814517141514)
+
+    async with aiosqlite.connect("memberslist.db") as db:
+        for m in mems.keys():
+            exists = await (
+                await db.execute("SELECT * FROM users WHERE uid = ?;", (m,))
+            ).fetchone()
+            if not exists:
+                await db.execute("INSERT INTO users VALUES (?);", (m,))
+        await db.commit()
+
+
+@greetings_plugin.listener(hikari.MemberCreateEvent)
+async def add_member(event: hikari.MemberCreateEvent):
+    async with aiosqlite.connect("memberslist.db") as db:
+        await db.execute("INSERT INTO users VALUES (?);", (id,))
+"""
 
 
 @greetings_plugin.listener(hikari.MemberCreateEvent)
@@ -10,17 +39,14 @@ async def on_member_join_update(event: hikari.MemberCreateEvent) -> None:
     after = event.member
     role = 993695690578464778
 
-    with open("bobert/db/users.json", "w+") as j:
-        j.write({str(event.member.id)})
-        
-        if role in [r.id for r in after.get_roles()] and role not in [
-            r.id for r in before.get_roles()
-        ]:
-            await greetings_plugin.bot.rest.create_message(
-                993567995936915536,
-                f"You made it {after.mention}! Welcome to **{event.member.get_guild().name}**, enjoy your stay 💚",
-                user_mentions=True,
-            )
+    if role in [r.id for r in after.get_roles()] and role not in [
+        r.id for r in before.get_roles()
+    ]:
+        await greetings_plugin.bot.rest.create_message(
+            993567995936915536,
+            f"You made it {after.mention}! Welcome to **{event.member.get_guild().name}**, enjoy your stay 💚",
+            user_mentions=True,
+        )
 
 
 def load(bot: lightbulb.BotApp) -> None:
