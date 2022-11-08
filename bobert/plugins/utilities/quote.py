cfa1ed41798cb1cc484b9bff14b350bf774146cd
st1xkz <83383474+st1xkz@users.noreply.github.com>
2022-11-08 05:21:19 +0000

    change int to str

--- bobert/plugins/utilities/quote.py
+++ bobert/plugins/utilities/quote.py
@@ -11,13 +11,13 @@ quote = lightbulb.Plugin("quote")
 @lightbulb.option(
     name="channel_id",
     description="channel id to get message from",
-    type=lightbulb.converters.special.GuildChannelConverter,
+    type=hikari.GuildChannel,
     required=True,
 )
 @lightbulb.option(
     name="message_id",
     description="the message to be quoted",
-    type=int,
+    type=str,
     required=True,
 )
 @lightbulb.command(
@@ -26,13 +26,14 @@ quote = lightbulb.Plugin("quote")
     pass_options=True,
 )
 @lightbulb.implements(lightbulb.SlashCommand)
-async def _quote(ctx: lightbulb.Context, message_id: int, channel_id: lightbulb.converters.special.GuildChannelConverter) -> None:
+async def _quote(ctx: lightbulb.Context, message_id: str, channel_id: hikari.GuildChannel) -> None:
+    _message_id = int(message_id)
     member = ctx.member
     color = (
         c[0] if (c := [r.color for r in member.get_roles() if r.color != 0]) else None
     )
 
-    message = await channel_id.fetch_message(message_id)
+    message = await channel_id.fetch_message(_message_id)
     guild_id = message.guild_id
     channel_id = message.channel_id
     message_id = message.id
