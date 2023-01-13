0219120e91d964f68871c9093c3df8605bf0fa3c
sticks <lucyabney101@gmail.com>
2023-01-13 06:20:53 +0000

    add class TicketButton starter thing

--- bobert/plugins/listeners/ticket.py
+++ bobert/plugins/listeners/ticket.py
@@ -1,7 +1,35 @@
 import hikari
 import lightbulb
+import miru
 
 ticket = lightbulb.Plugin("ticket")
+
+
+class TicketButton(miru.View):
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
+
+    """
+    Start support button interaction for TicketButton view
+
+    Button params:
+        label - button label
+        style - button style
+        function - support_button
+
+    Check if interaction member already has an open ticket in db.
+    If not exists, start new ticket process and add ID to db.
+    Returns discord ui Modal to take in ticket summary
+
+    Params:
+        button - miru button
+        interaction - miru interaction
+    """
+
+    @miru.button(label="Start Support", style=hikari.ButtonStyle.PRIMARY)
+    async def support_button(self, button: miru.Button, ctx: miru.ViewContext) -> None:
+        # Get open ticket list to check if member already has an open ticket
+        ...
 
 
 @ticket.command
