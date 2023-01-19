7e9671ef1a282f85d9d73ed6e9d91f4b5274f528
sticks <lucyabney101@gmail.com>
2023-01-19 04:32:24 +0000

    str?

--- bobert/plugins/information/member.py
+++ bobert/plugins/information/member.py
@@ -106,7 +106,7 @@ async def _user(ctx: lightbulb.Context, member: hikari.Member) -> None:
         )
         .add_field(
             "Activity",
-            f"{get_status}",
+            f"{str(get_status)}",
             inline=False,
         )
         .add_field(
